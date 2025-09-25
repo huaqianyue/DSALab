f9cec5a39cef6e2e4ceedfbdaedd170a8802cae6
Xingyu Wang <WMX_0121@outlook.com>
2025-09-25 09:38:45 +0800

    删除不必要的

--- test_utf8.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-
-int main() {
-    std::cout << "调试控制台编码测试 - Hello World" << std::endl;
-    std::cout << "Encoding: utf8" << std::endl;
-    std::cout << "This is a test program for encoding verification." << std::endl;
-    return 0;
-}
